3232fb3498ab24a8f26ff3b2a07a3151ec6d5096
MakhrovSS <stanisav.makhrov@gmail.com>
2019-08-06 12:42:30 +0300

    changed DB code

--- src/main/resources/init_DB.sql
+++ src/main/resources/init_DB.sql
@@ -8,12 +8,12 @@ CREATE TABLE doctype
 
 CREATE TABLE doc
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  id_doctype       INTEGER                 NOT NULL,
-  reg_num          VARCHAR                 NOT NULL,
-  reg_date         TIMESTAMP               NOT NULL,
-  insert_date      TIMESTAMP DEFAULT now() NOT NULL,
-  FOREIGN KEY (id_doctype) REFERENCES doctype (id) ON DELETE CASCADE
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    id_doctype       INTEGER                 NOT NULL,
+    reg_num          VARCHAR                 NOT NULL,
+    reg_date         TIMESTAMP               NOT NULL,
+    insert_date      TIMESTAMP DEFAULT now() NOT NULL,
+    FOREIGN KEY (id_doctype) REFERENCES doctype (id) ON DELETE CASCADE
 );
 
 CREATE TABLE fieldtype
@@ -24,24 +24,31 @@ CREATE TABLE fieldtype
 
 CREATE TABLE field
 (
-  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name             VARCHAR                 NOT NULL,
-  id_fieldtype     INTEGER                 NOT NULL,
-  length           INTEGER                 NOT NULL,
-  FOREIGN KEY (id_fieldtype) REFERENCES fieldtype (id) ON DELETE CASCADE
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name             VARCHAR                 NOT NULL,
+    id_fieldtype     INTEGER                 NOT NULL,
+    length           INTEGER                 NOT NULL,
+    FOREIGN KEY (id_fieldtype) REFERENCES fieldtype (id) ON DELETE CASCADE
+);
+
+CREATE TABLE groupedfield
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    name             VARCHAR
 );
 
 CREATE TABLE doctypefields
 (
-  id_doctype       INTEGER                 NOT NULL,
-  id_field         INTEGER                 NOT NULL,
-  position         INTEGER                 NOT NULL,
-  positionin_group INTEGER                         ,
-  group_num        INTEGER                         ,
-  max_count        INTEGER                         ,
-  FOREIGN KEY (id_doctype) REFERENCES doctype (id) ON DELETE CASCADE,
-  FOREIGN KEY (id_field) REFERENCES field (id) ON DELETE CASCADE,
-  CONSTRAINT c_doctypefields UNIQUE (id_doctype, id_field, group_num)
+    id_doctype       INTEGER                 NOT NULL,
+    id_field         INTEGER                 NOT NULL,
+    position         INTEGER                 NOT NULL,
+    groupedfield_id  INTEGER                         ,
+    positionin_group INTEGER                         ,
+    max_count        INTEGER                         ,
+    CONSTRAINT doctype_fields UNIQUE (id_doctype, id_field, groupedfield_id),
+    FOREIGN KEY (id_doctype) REFERENCES doctype (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_field) REFERENCES field (id) ON DELETE CASCADE,
+    FOREIGN KEY (groupedfield_id) REFERENCES groupedfield (id) ON DELETE CASCADE
 );
 
 CREATE TABLE var
@@ -50,7 +57,6 @@ CREATE TABLE var
     name             INTEGER                 NOT NULL,
     vartype          INTEGER                 NOT NULL
 );
-
 
 CREATE TABLE varelem
 (
@@ -63,17 +69,20 @@ CREATE TABLE varelem
 
 CREATE TABLE docfields
 (
-  id_doc                INTEGER                 NOT NULL,
-  id_field              INTEGER                 NOT NULL,
-  position_fact         INTEGER                 NOT NULL,
-  positionin_group      INTEGER                         ,
-  group_num             INTEGER                         ,
-  int_value             INTEGER                         ,
-  str_value             VARCHAR                         ,
-  date_value            TIMESTAMP                       ,
-  id_var                INTEGER                         ,
-  CONSTRAINT c_docfields UNIQUE (id_doc, id_field),
-  FOREIGN KEY (id_var) REFERENCES var (id) ON DELETE CASCADE,
-  FOREIGN KEY (id_doc) REFERENCES doc (id) ON DELETE CASCADE,
-  FOREIGN KEY (id_field) REFERENCES field (id) ON DELETE CASCADE
+    id_doc                INTEGER                 NOT NULL,
+    id_field              INTEGER                 NOT NULL,
+    position_fact         INTEGER                 NOT NULL,
+    positionin_group_fact INTEGER                         ,
+    groupedfield_id       INTEGER                         ,
+    int_value             INTEGER                         ,
+    str_value             VARCHAR                         ,
+    date_value            TIMESTAMP                       ,
+    id_var                INTEGER                         ,
+    CONSTRAINT doc_fields UNIQUE (id_doc, id_field),
+    FOREIGN KEY (id_doc) REFERENCES doc (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_field) REFERENCES field (id) ON DELETE CASCADE,
+    FOREIGN KEY (id_var) REFERENCES var (id) ON DELETE CASCADE,
+    FOREIGN KEY (groupedfield_id) REFERENCES groupedfield (id) ON DELETE CASCADE
 );
+
+
