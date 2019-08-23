1a8516a309d9b9ece4e2c4db02a5ac848f3b1536
StanislavMakhrov <SlavaM4@yandex.ru>
2019-08-23 11:25:56 +0300

    pdf return corrected

--- src/main/java/ru/gbuac/service/DocServiceImpl.java
+++ src/main/java/ru/gbuac/service/DocServiceImpl.java
@@ -13,6 +13,10 @@ import ru.gbuac.to.FieldTo;
 import ru.gbuac.util.*;
 import ru.gbuac.util.exception.NotFoundException;
 import ru.gbuac.util.exception.UnauthorizedUserException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -46,7 +50,7 @@ public class DocServiceImpl implements DocService {
     private RoleRepository roleRepository;
 
     @Value("${pdf.final.dir}")
-    private String pdfFinalDir;
+    private String pdfDir;
 
     @Value("${pdf.temp.dir}")
     private String pdfTempDir;
@@ -59,6 +63,178 @@ public class DocServiceImpl implements DocService {
         return checkNotFoundWithId(docRepository.findById(id).orElse(null), id);
     }
 
+    @Override
+    public DocTo getFullByUserName(int id, String userName) throws NotFoundException {
+        return asDocTo(checkNotFoundWithId(docRepository.findById(id).orElse(null), id), userName);
+    }
+
+    @Override
+    public List<Doc> getAllAgreementByUsername(String userName) {
+        List<Doc> viewDocs = new ArrayList<>();
+        for (DocTypeRoutes docTypeRoute: docTypeRoutesRepository.getByUserName(userName)) {
+            viewDocs.addAll(docRepository.getAllAgreementByDocTypeAndStage(
+                    docTypeRoute.getDocType().getId(), docTypeRoute.getAgreeStage()));
+        }
+        return viewDocs;
+    }
+
+    @Override
+    public List<Doc> getAll() {
+        return docRepository.getAll();
+    }
+
+    private Doc prepareToPersist(Doc doc) {
+        Integer currentAgreementStage = doc.getCurrentAgreementStage();
+        Integer finalStageForThisDocType = docTypeRoutesRepository.getFinalStageForDocType(doc.getDocType().getId());
+        if (doc.getRegNum() == null) {
+            if (currentAgreementStage == null) {
+                doc.setCurrentAgreementStage(1);
+            } else {
+                if (currentAgreementStage != finalStageForThisDocType)
+                    doc.setCurrentAgreementStage(currentAgreementStage + 1);
+            }
+        }
+        return doc;
+    }
+
+    @Override
+    public DocTo save(DocTo docTo, String userName, String rootPath) throws UnauthorizedUserException {
+        Assert.notNull(docTo, "doc must not be null");
+        List<DocTypeFields> docTypeFields = docTypeFieldsRepository.getAll(docTo.getDocTypeId());
+        List<Role> curUserRoles = roleRepository.getRolesByUsername(userName);
+
+        Long existRole = 0L;
+        for (DocFieldsTo d:docTo.getChildFields()) {
+            existRole += docTypeFields.stream()
+                    .filter(f -> f.getField().getId() == d.getField().getFieldId())
+                    .filter(f -> curUserRoles.contains(f.getRole())).count();
+        }
+        if (existRole == 0) {
+            throw new UnauthorizedUserException();
+        }
+
+        if (docTo.isFinalStage() != null && docTo.isFinalStage()) {
+            docTo.setRegNum("ДЭПР-" + new Random().nextInt(100) + "/19");
+            docTo.setRegDateTime(LocalDateTime.now());
+        }
+        else {
+            docTo.setProjectRegNum("согл-"+ new Random().nextInt(100)+"/19");
+        }
+        DocTo saved = asDocTo(docRepository.save(prepareToPersist(createNewDocFromTo(docTo))), userName);
+        String urlPdf = createPdf(saved, rootPath, false);
+        docRepository.setUrlPDF(saved.getId(), urlPdf);
+        saved.setUrlPDF(urlPdf);
+        return saved;
+    }
+
+    @Override
+    public DocTo update(DocTo docTo, int id, String userName, String rootPath) throws NotFoundException, UnauthorizedUserException {
+        Assert.notNull(docTo, "docTo must not be null");
+        Doc updated = docFromTo(docTo);
+        docValuedFieldsRepository.deleteAll(id);
+        DocTo updatedTo = asDocTo(checkNotFoundWithId(docRepository.save(prepareToPersist(updated)), id), userName);
+        updatedTo.setUrlPDF(createPdf(updatedTo, rootPath, false));
+        return updatedTo;
+    }
+
+    @Override
+    public void delete(int id) throws NotFoundException {
+        Assert.notNull(id, "doc must not be null");
+        checkNotFoundWithId(docRepository.delete(id)!= 0, id);
+    }
+
+    private void fillTags(FieldTo fieldTo, Map<String, String> simpleTags, Map<String, TaggedTable> taggedTables) {
+        String tag = fieldTo.getTag();
+        if (TagUtil.getTableTag(tag) != null) {
+            Map<String, String> cellsTags;
+            String tableTag = TagUtil.getTableTag(tag);
+            if (!taggedTables.containsKey(tableTag)) {
+                cellsTags = new HashMap<>();
+                taggedTables.put(tableTag, new TaggedTable(tableTag, new ArrayList<TableRow>() {{
+                    add(new TableRow(cellsTags));}}));
+            } else {
+                List<TableRow> rows = taggedTables.get(tableTag).getRows();
+                cellsTags = taggedTables.get(tableTag).getRows().get(rows.size()-1).getCellsTags();
+            }
+            switch (fieldTo.getFieldType()) {
+                case GROUP_FIELDS:
+                    for (FieldTo childField : fieldTo.getChildFields()) {
+                        fillTags(childField, simpleTags, taggedTables);
+                    }
+                    break;
+                case CATALOG:
+                    CatalogElem catalogElemChild =
+                            catalogElemRepository.findById(fieldTo.getValueInt()).orElse(null);
+                    switch (catalogElemChild.getCatalog().getCatalogType()) {
+                        case TEXT:
+                            cellsTags.put(tag, catalogElemChild.getValueStr());
+                            break;
+                        case NUMBER:
+                            cellsTags.put(tag, catalogElemChild.getValueInt());
+                            break;
+                    }
+                    break;
+                default:
+                    cellsTags.put(tag, fieldTo.getValueByFieldType());
+                    break;
+            }
+        } else
+        if (TagUtil.getSimpleTag(tag) != null) {
+            switch (fieldTo.getFieldType()) {
+                case GROUP_FIELDS:
+                    for (FieldTo childField : fieldTo.getChildFields()) {
+                        fillTags(childField, simpleTags, taggedTables);
+                    }
+                    break;
+                case CATALOG:
+                    CatalogElem catalogElemChild =
+                            catalogElemRepository.findById(fieldTo.getValueInt()).orElse(null);
+                    switch (catalogElemChild.getCatalog().getCatalogType()) {
+                        case TEXT:
+                            simpleTags.put(tag, catalogElemChild.getValueStr());
+                            break;
+                        case NUMBER:
+                            simpleTags.put(tag, catalogElemChild.getValueInt());
+                            break;
+                    }
+                default:
+                    simpleTags.put(tag, fieldTo.getValueByFieldType());
+                    break;
+            }
+        }
+
+    }
+
+    @Override
+    public String getPdfPathByDocTo(DocTo docTo, String rootPath) {
+        return createPdf(asDocTo(docFromTo(docTo), null), rootPath, true);
+    }
+
+    private String createPdf(DocTo docTo, String rootPath, Boolean saveToTempDir) {
+        String templatePath = rootPath + docTemplatesDir + "Templ.docx";
+        String pdfTempPath = rootPath + pdfTempDir + docTo.getId() + ".pdf";
+        String pdfPath = rootPath + pdfDir + docTo.getId() + ".pdf";
+        String savePath = saveToTempDir ? pdfTempPath : pdfPath;
+
+        Map<String, String> simpleTags = new HashMap<>();
+        simpleTags.put("RegNum", docTo.getRegNum() != null ? docTo.getRegNum() : docTo.getProjectRegNum());
+        Map<String, TaggedTable> taggedTables = new HashMap<>();
+
+        for (DocFieldsTo docFieldsTo : docTo.getChildFields()) {
+            fillTags(docFieldsTo.getField(), simpleTags, taggedTables);
+        }
+
+        try {
+            ByteArrayOutputStream byteArrayOutputStream =
+                    Templater.fillTagsByDictionary(templatePath, simpleTags, taggedTables,
+                    pdfTempPath, true);
+            byteArrayOutputStream.writeTo(new FileOutputStream(savePath));
+        } catch (Exception e) {
+        }
+
+        return savePath.replace(rootPath,"");
+    }
+
     private DocTo asDocTo(Doc doc, String userName) {
         Integer docTypeId = doc.getDocType().getId();
 
@@ -66,7 +242,7 @@ public class DocServiceImpl implements DocService {
         Boolean isFinalStage = docTypeRoutesRepository.getFinalStageForDocType(docTypeId) == curAgreementStage;
 
         List<Role> curUserRoles = roleRepository.getRolesByUsername(userName);
-        List<DocValuedFields> docValuedFields = docValuedFieldsRepository.getAll(doc.getId());
+        List<DocValuedFields> docValuedFields = doc.getDocValuedFields();
         List<DocTypeFields> docTypeFields = docTypeFieldsRepository.getAll(docTypeId);
         List<DocFieldsTo> docFieldsTos = new ArrayList<>();
 
@@ -134,168 +310,4 @@ public class DocServiceImpl implements DocService {
         return doc;
     }
 
-    @Override
-    public DocTo getFullByUserName(int id, String userName) throws NotFoundException {
-        return asDocTo(checkNotFoundWithId(docRepository.findById(id).orElse(null), id), userName);
-    }
-
-    @Override
-    public List<Doc> getAllAgreementByUsername(String userName) {
-        List<Doc> viewDocs = new ArrayList<>();
-        for (DocTypeRoutes docTypeRoute: docTypeRoutesRepository.getByUserName(userName)) {
-            viewDocs.addAll(docRepository.getAllAgreementByDocTypeAndStage(
-                    docTypeRoute.getDocType().getId(), docTypeRoute.getAgreeStage()));
-        }
-        return viewDocs;
-    }
-
-    @Override
-    public List<Doc> getAll() {
-        return docRepository.getAll();
-    }
-
-    private Doc prepareToPersist(Doc doc) {
-        Integer currentAgreementStage = doc.getCurrentAgreementStage();
-        Integer finalStageForThisDocType = docTypeRoutesRepository.getFinalStageForDocType(doc.getDocType().getId());
-        if (doc.getRegNum() == null) {
-            if (currentAgreementStage == null) {
-                doc.setCurrentAgreementStage(1);
-            } else {
-                if (currentAgreementStage != finalStageForThisDocType)
-                    doc.setCurrentAgreementStage(currentAgreementStage + 1);
-            }
-        }
-        return doc;
-    }
-
-    @Override
-    public DocTo save(DocTo docTo, String userName, String rootPath) throws UnauthorizedUserException {
-        Assert.notNull(docTo, "doc must not be null");
-        List<DocTypeFields> docTypeFields = docTypeFieldsRepository.getAll(docTo.getDocTypeId());
-        List<Role> curUserRoles = roleRepository.getRolesByUsername(userName);
-
-        Long existRole = 0L;
-        for (DocFieldsTo d:docTo.getChildFields()) {
-            existRole += docTypeFields.stream()
-                    .filter(f -> f.getField().getId() == d.getField().getFieldId())
-                    .filter(f -> curUserRoles.contains(f.getRole())).count();
-        }
-        if (existRole == 0) {
-            throw new UnauthorizedUserException();
-        }
-
-        if (docTo.isFinalStage() != null && docTo.isFinalStage()) {
-            docTo.setRegNum("ДЭПР-" + new Random().nextInt(100) + "/19");
-            docTo.setRegDateTime(LocalDateTime.now());
-        }
-        else {
-            docTo.setProjectRegNum("согл-"+ new Random().nextInt(100)+"/19");
-        }
-        DocTo saved = asDocTo(docRepository.save(prepareToPersist(createNewDocFromTo(docTo))), userName);
-        String urlPdf = getPdfPathByDocTo(saved, rootPath);
-        docRepository.setUrlPDF(saved.getId(), urlPdf);
-        saved.setUrlPDF(urlPdf);
-        return saved;
-    }
-
-    @Override
-    public DocTo update(DocTo docTo, int id, String userName, String rootPath) throws NotFoundException, UnauthorizedUserException {
-        Assert.notNull(docTo, "docTo must not be null");
-        Doc updated = docFromTo(docTo);
-        docValuedFieldsRepository.deleteAll(id);
-        DocTo updatedTo = asDocTo(checkNotFoundWithId(docRepository.save(prepareToPersist(updated)), id), userName);
-        updatedTo.setUrlPDF(getPdfPathByDocTo(updatedTo, rootPath));
-        return updatedTo;
-    }
-
-    @Override
-    public void delete(int id) throws NotFoundException {
-        Assert.notNull(id, "doc must not be null");
-        checkNotFoundWithId(docRepository.delete(id)!= 0, id);
-    }
-
-    public void fillTags(FieldTo fieldTo, Map<String, String> simpleTags, Map<String, TaggedTable> taggedTables) {
-        String tag = fieldTo.getTag();
-        if (TagUtil.getTableTag(tag) != null) {
-            Map<String, String> cellsTags;
-            String tableTag = TagUtil.getTableTag(tag);
-            if (!taggedTables.containsKey(tableTag)) {
-                cellsTags = new HashMap<>();
-                taggedTables.put(tableTag, new TaggedTable(tableTag, new ArrayList<TableRow>() {{
-                    add(new TableRow(cellsTags));}}));
-            } else {
-                List<TableRow> rows = taggedTables.get(tableTag).getRows();
-                cellsTags = taggedTables.get(tableTag).getRows().get(rows.size()-1).getCellsTags();
-            }
-            switch (fieldTo.getFieldType()) {
-                case GROUP_FIELDS:
-                    for (FieldTo childField : fieldTo.getChildFields()) {
-                        fillTags(childField, simpleTags, taggedTables);
-                    }
-                    break;
-                case CATALOG:
-                    CatalogElem catalogElemChild =
-                            catalogElemRepository.findById(fieldTo.getValueInt()).orElse(null);
-                    switch (catalogElemChild.getCatalog().getCatalogType()) {
-                        case TEXT:
-                            cellsTags.put(tag, catalogElemChild.getValueStr());
-                            break;
-                        case NUMBER:
-                            cellsTags.put(tag, catalogElemChild.getValueInt());
-                            break;
-                    }
-                    break;
-                default:
-                    cellsTags.put(tag, fieldTo.getValueByFieldType());
-                    break;
-            }
-        } else
-        if (TagUtil.getSimpleTag(tag) != null) {
-            switch (fieldTo.getFieldType()) {
-                case GROUP_FIELDS:
-                    for (FieldTo childField : fieldTo.getChildFields()) {
-                        fillTags(childField, simpleTags, taggedTables);
-                    }
-                    break;
-                case CATALOG:
-                    CatalogElem catalogElemChild =
-                            catalogElemRepository.findById(fieldTo.getValueInt()).orElse(null);
-                    switch (catalogElemChild.getCatalog().getCatalogType()) {
-                        case TEXT:
-                            simpleTags.put(tag, catalogElemChild.getValueStr());
-                            break;
-                        case NUMBER:
-                            simpleTags.put(tag, catalogElemChild.getValueInt());
-                            break;
-                    }
-                default:
-                    simpleTags.put(tag, fieldTo.getValueByFieldType());
-                    break;
-            }
-        }
-
-    }
-
-    @Override
-    public String getPdfPathByDocTo(DocTo docTo, String rootPath) {
-        String templatePath = rootPath + docTemplatesDir + "Templ.docx";
-
-        Map<String, String> simpleTags = new HashMap<>();
-        simpleTags.put("RegNum", docTo.getRegNum() != null ? docTo.getRegNum() : docTo.getProjectRegNum());
-        Map<String, TaggedTable> taggedTables = new HashMap<>();
-
-        for (DocFieldsTo docFieldsTo : docTo.getChildFields()) {
-            fillTags(docFieldsTo.getField(), simpleTags, taggedTables);
-        }
-
-        String pdfTempPath = rootPath + pdfTempDir + "povestka_test1.pdf";
-        try {
-            Templater.fillTagsByDictionary(templatePath, simpleTags, taggedTables,
-                    pdfTempPath, true);
-        } catch (Exception e) {
-            pdfTempPath = null;
-        }
-
-        return pdfTempPath.replace(rootPath,"");
-    }
 }
