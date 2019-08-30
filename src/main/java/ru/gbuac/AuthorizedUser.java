4d60972d87a1fd2eec25c610a188ae4ac4864b45
StanislavMakhrov <SlavaM4@yandex.ru>
2019-08-30 09:42:04 +0300

    corrected roles

--- src/main/java/ru/gbuac/AuthorizedUser.java
+++ src/main/java/ru/gbuac/AuthorizedUser.java
@@ -43,7 +43,8 @@ public class AuthorizedUser {
 
     public static boolean hasRole(String role) {
         return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority).collect(Collectors.toList()).contains("ROLE_" + role);
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList()).contains(!role.contains("ROLE_") ? "ROLE_" + role : role);
     }
 
     private static LdapUserDetailsImpl get() {
