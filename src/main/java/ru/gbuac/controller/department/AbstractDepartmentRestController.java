2fdc17d5152445a5326f55b8666c40a433fbd7d1
StanislavMakhrov <SlavaM4@yandex.ru>
2019-10-11 11:00:45 +0300

    added departments

---
+++ src/main/java/ru/gbuac/controller/department/AbstractDepartmentRestController.java
@@ -0,0 +1,51 @@
+package ru.gbuac.controller.department;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.gbuac.model.Department;
+import ru.gbuac.service.DepartmentService;
+
+import java.util.List;
+
+import static ru.gbuac.util.ValidationUtil.assureIdConsistent;
+import static ru.gbuac.util.ValidationUtil.checkNew;
+
+public abstract class AbstractDepartmentRestController {
+    protected final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DepartmentService departmentService;
+
+    public Department get(int id) {
+        LOG.info("get " + id);
+        return departmentService.get(id);
+    }
+
+    public List<Department> getAll() {
+        LOG.info("getAll");
+        return departmentService.getAll();
+    }
+
+    public Department create(Department department) {
+        LOG.info("create " + department);
+        checkNew(department);
+        return departmentService.save(department);
+    }
+
+    public Department update(Department department, int id) {
+        LOG.info("update " + department);
+        assureIdConsistent(department, id);
+        return departmentService.update(department, id);
+    }
+
+    public void delete(int id) {
+        LOG.info("delete " + id);
+        departmentService.delete(id);
+    }
+
+    public List<Department> getAllTopLevel() {
+        LOG.info("getAllTopLevelDepartment");
+        return departmentService.getAllTopLevel();
+    }
+}
---
+++ src/main/java/ru/gbuac/controller/department/DepartmentRestController.java
@@ -0,0 +1,39 @@
+package ru.gbuac.controller.department;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.gbuac.model.Department;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = DepartmentRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class DepartmentRestController extends AbstractDepartmentRestController{
+    public static final String REST_URL = "/rest/profile/departments";
+
+    @Override
+    @GetMapping
+    public List<Department> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Department updateOrCreate(@Valid @RequestBody Department department) {
+        if (department.isNew()) {
+            return super.create(department);
+        } else {
+            return super.update(department, department.getId());
+        }
+    }
+
+    @Override
+    @GetMapping(value = "/getAllTopLevel")
+    public List<Department> getAllTopLevel() {
+        return super.getAllTopLevel();
+    }
+}
