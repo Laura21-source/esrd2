5f55c36ebbb7d83b52d4a2830e869e8c108d6356
StanislavMakhrov <stanislav.makhrov@gmail.com>
2020-01-26 21:05:59 +0300

    Corrected PublishDataService

--- src/main/java/ru/gbuac/controller/publishdata/PublishDataRestController.java
+++ src/main/java/ru/gbuac/controller/publishdata/PublishDataRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.gbuac.model.PublishData;
 
@@ -12,13 +13,13 @@ import javax.servlet.ServletContext;
 @RestController
 @RequestMapping(value = PublishDataRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class PublishDataRestController extends AbstractPublishDataRestController {
-    public static final String REST_URL = "/rest/admin/docs//publishdata";
+    public static final String REST_URL = "/rest/admin/publishdata";
 
     @Autowired
     ServletContext context;
 
-    @PostMapping(value = "/rejectDocAgreement/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    PublishData publish(int docId) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    PublishData publish(@RequestParam("docId") int docId) {
         return super.publish(docId, context.getRealPath("/"));
     }
 }
